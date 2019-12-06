bb097683f5fa3d3e8acd51e59ec462c641376d01
vincebenkert <vpbenker@gmail.com>
2019-12-06 13:10:27 -0500

    Create AOC_DayFive_Puzzle2.js

---
+++ AOC_DayFive_Puzzle2.js
@@ -0,0 +1,303 @@
+var inputArray = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 35, 92, 225, 1101, 25, 55, 225, 1102, 47, 36, 225, 1102, 17, 35, 225, 1, 165, 18, 224, 1001, 224, -106, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 3, 224, 1, 223, 224, 223, 1101, 68, 23, 224, 101, -91, 224, 224, 4, 224, 102, 8, 223, 223, 101, 1, 224, 224, 1, 223, 224, 223, 2, 217, 13, 224, 1001, 224, -1890, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 6, 224, 1, 224, 223, 223, 1102, 69, 77, 224, 1001, 224, -5313, 224, 4, 224, 1002, 223, 8, 223, 101, 2, 224, 224, 1, 224, 223, 223, 102, 50, 22, 224, 101, -1800, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 5, 224, 1, 224, 223, 223, 1102, 89, 32, 225, 1001, 26, 60, 224, 1001, 224, -95, 224, 4, 224, 102, 8, 223, 223, 101, 2, 224, 224, 1, 223, 224, 223, 1102, 51, 79, 225, 1102, 65, 30, 225, 1002, 170, 86, 224, 101, -2580, 224, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 6, 224, 1, 223, 224, 223, 101, 39, 139, 224, 1001, 224, -128, 224, 4, 224, 102, 8, 223, 223, 101, 3, 224, 224, 1, 223, 224, 223, 1102, 54, 93, 225, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 1008, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 329, 101, 1, 223, 223, 7, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 344, 101, 1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 359, 1001, 223, 1, 223, 7, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 374, 1001, 223, 1, 223, 1107, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 389, 1001, 223, 1, 223, 107, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 404, 1001, 223, 1, 223, 1108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 419, 101, 1, 223, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 434, 1001, 223, 1, 223, 108, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 449, 101, 1, 223, 223, 108, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 464, 1001, 223, 1, 223, 1007, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 479, 101, 1, 223, 223, 8, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 494, 101, 1, 223, 223, 1007, 226, 677, 224, 102, 2, 223, 223, 1006, 224, 509, 101, 1, 223, 223, 7, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 524, 101, 1, 223, 223, 107, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 539, 101, 1, 223, 223, 1008, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 554, 1001, 223, 1, 223, 1008, 226, 226, 224, 1002, 223, 2, 223, 1006, 224, 569, 1001, 223, 1, 223, 1108, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 584, 101, 1, 223, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 599, 1001, 223, 1, 223, 8, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 614, 1001, 223, 1, 223, 1108, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 629, 1001, 223, 1, 223, 8, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 644, 1001, 223, 1, 223, 1107, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 659, 1001, 223, 1, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 674, 101, 1, 223, 223, 4, 223, 99, 226];
+
+var inputArray2 = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]; //passed....
+
+var inputArray3 =[3,9,8,9,10,9,4,9,99,-1,8]; //passed
+
+var inputArray4 = [3,9,7,9,10,9,4,9,99,-1,8]; //passed....
+
+var inputArray5 = [3,3,1108,-1,8,3,4,3,99];  //passed....
+
+var inputArray6 = [3,3,1107,-1,8,3,4,3,99]; //PASSED....
+
+var inputArray7 = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]; // Passed...
+
+
+
+
+
+
+
+
+var finalValue = 0;
+var posToReplace = 0;
+var valPositionOne = 0;
+var valPositionTwo = 0;
+var currentPos = 0;
+var notEvaluated = true;
+var input = 5;
+var optCode;
+
+var parameterOne = "0";
+var parameterTwo = "0";
+var parameterThree = "0";
+var parameterFour = "0";
+
+function breakdown(optCode) {
+  var newoptCode = optCode.toString();
+  console.log("Current optCode = " + optCode);
+  var newoptCodeLength = newoptCode.length;
+  //console.log("OptCode length = " + newoptCodeLength);
+
+
+  if (newoptCode[newoptCodeLength - 3])
+    parameterOne = newoptCode[newoptCodeLength - 3];
+
+  if (newoptCode[newoptCodeLength - 4])
+    parameterTwo = newoptCode[newoptCodeLength - 4];
+
+  if (newoptCode[newoptCodeLength - 5])
+    parameterThree = newoptCode[newoptCodeLength - 5];
+
+  if (newoptCode[newoptCodeLength - 6])
+    parameterFour = newoptCode[newoptCodeLength - 6];
+
+  if (newoptCodeLength > 1)
+    optCode = newoptCode[newoptCodeLength - 2] + newoptCode[newoptCodeLength - 1];
+  else
+    optCode = "0" + newoptCode[newoptCodeLength - 1];
+
+  //console.log("optCode= "+ optCode + " Parameter 1 = " + parameterOne + " Parameter 2 = " + parameterTwo + " Parameter 3 = " + parameterThree + " ParameterFour = " + parameterFour )   
+  // console.log("Current optCode after EVAL= " + optCode);
+
+
+  return optCode;
+}
+
+while (inputArray[currentPos] != "99") {
+  notEvaluated = true;
+  //Breakdown OptCode / Parameters...
+  optCode = breakdown(inputArray[currentPos]);
+  //console.log("In while..  optCode = " + optCode);
+  console.log("optCode= " + optCode + " Parameter 1 = " + parameterOne + " Parameter 2 = " + parameterTwo + " Parameter 3 = " + parameterThree + " ParameterFour = " + parameterFour);
+  // OptCode 5
+  if (notEvaluated == true) {
+    if (optCode == "05") {
+      console.log("made it to 5");
+      if (parameterOne == "0")
+        valPositionOne = inputArray[currentPos + 1];
+      else
+        valPositionOne = currentPos + 1;
+
+      if (parameterTwo == "0")
+        valPositionTwo = inputArray[currentPos + 2];
+      else
+        valPositionTwo = currentPos + 2;
+
+
+      if (inputArray[valPositionOne] != 0) {
+        //if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter...
+        currentPos = inputArray[valPositionTwo];
+        notEvaluated = false;
+        // currentPos = currentPos + 3;
+      } else {
+        console.log("Do Nothing because Parameter One = 0 " + inputArray[valPositionOne]);
+        notEvaluated = false;
+        currentPos = currentPos + 3;
+      }
+    }
+  }
+
+  //OptCode 6  
+  if (notEvaluated == true) {
+    if (optCode == "06") {
+      console.log("made it to 6");
+      if (parameterOne == "0")
+        valPositionOne = inputArray[currentPos + 1];
+      else
+        valPositionOne = currentPos + 1;
+
+      if (parameterTwo == "0")
+        valPositionTwo = inputArray[currentPos + 2];
+      else{
+      	console.log("ParameterTwoMode not zero...");
+        valPositionTwo = currentPos + 2;
+			}
+      if (inputArray[valPositionOne] == 0) {
+        //if the first parameter is zero, it sets the instruction pointer to the value from the second parameter.
+        console.log("First Parameter was ZERO changing Currert instruction pointer to " + inputArray[valPositionTwo]);
+        console.log("Values of either side of " + currentPos + " & " +  valPositionTwo  );
+        currentPos = inputArray[valPositionTwo];
+        notEvaluated = false;
+        //currentPos = currentPos + 3;
+      } else {
+        console.log("Do Nothing because ParameterOne = 0");
+        notEvaluated = false;
+        currentPos = currentPos + 3;
+      }
+    }
+  }
+  
+  //OPTCODE 7
+  if (notEvaluated == true) {
+    if (optCode == "07") {
+      console.log("made it to 7");
+      if (parameterOne == "0")
+        valPositionOne = inputArray[currentPos + 1];
+      else
+        valPositionOne = currentPos + 1;
+
+      if (parameterTwo == "0")
+        valPositionTwo = inputArray[currentPos + 2];
+      else
+        valPositionTwo = currentPos + 2;
+
+      if (parameterThree == "0")
+        posToReplace = inputArray[currentPos + 3];
+      else
+        posToReplace = currentPos + 3;
+
+
+
+      if (inputArray[valPositionOne] < inputArray[valPositionTwo])
+        inputArray[posToReplace] = 1;
+      else
+        inputArray[posToReplace] = 0;
+
+      notEvaluated = false;
+      currentPos = currentPos + 4;
+    }
+  }
+
+  //OptCode 08
+  if (notEvaluated == true) {
+    if (optCode == "08") {
+      console.log("made it to 8");
+      if (parameterOne == "0")
+        valPositionOne = inputArray[currentPos + 1];
+      else
+        valPositionOne = currentPos + 1;
+
+      if (parameterTwo == "0")
+        valPositionTwo = inputArray[currentPos + 2];
+      else
+        valPositionTwo = currentPos + 2;
+
+      if (parameterThree == "0")
+        posToReplace = inputArray[currentPos + 3];
+      else
+        posToReplace = currentPos + 3;
+
+
+      console.log(inputArray[valPositionOne] + " vs " + inputArray[valPositionTwo]);
+      if (inputArray[valPositionOne] == inputArray[valPositionTwo]){
+        inputArray[posToReplace] = 1;
+        console.log(" EVAL1 " + inputArray[valPositionOne] + " Eval2 " + inputArray[valPositionTwo] + " put ONE in position " + posToReplace);
+        }
+      else{
+      	console.log("CURERNT POS " + currentPos + " + 3  -  Previous value was ... " + inputArray[5] );
+      	console.log(" EVAL1 " + inputArray[valPositionOne] + " Eval2 " + inputArray[valPositionTwo] + " put ZERO in position " + posToReplace);
+        inputArray[posToReplace] = 0;
+				}
+      notEvaluated = false;
+      currentPos = currentPos + 4;
+    }
+  }
+
+
+  if (notEvaluated == true) {
+    if (optCode == "01") {
+
+      if (parameterOne == "0")
+        valPositionOne = inputArray[currentPos + 1];
+      else
+        valPositionOne = currentPos + 1;
+
+      if (parameterTwo == "0")
+        valPositionTwo = inputArray[currentPos + 2];
+      else
+        valPositionTwo = currentPos + 2;
+
+      if (parameterThree == "0")
+        posToReplace = inputArray[currentPos + 3];
+      else
+        posToReplace = currentPos + 3;
+
+      console.log("posToReplace= " + posToReplace);
+      inputArray[posToReplace] = inputArray[valPositionOne] + inputArray[valPositionTwo];
+      console.log("Replacing value with = " + inputArray[posToReplace]);
+      //time to move on so...
+      console.log("made it to 1");
+      notEvaluated = false;
+      currentPos = currentPos + 4;
+    }
+  }
+  if (notEvaluated == true) {
+    if (optCode == "02") {
+      if (parameterOne == "0")
+        valPositionOne = inputArray[currentPos + 1];
+      else
+        valPositionOne = currentPos + 1;
+
+      if (parameterTwo == "0")
+        valPositionTwo = inputArray[currentPos + 2];
+      else
+        valPositionTwo = currentPos + 2;
+
+      if (parameterThree == "0")
+        posToReplace = inputArray[currentPos + 3];
+      else
+        posToReplace = currentPos + 3;
+      console.log("posToReplace= " + posToReplace);
+      inputArray[posToReplace] = inputArray[valPositionOne] * inputArray[valPositionTwo];
+      //time to move on so...
+      console.log("made it to 2");
+      notEvaluated = false;
+      currentPos = currentPos + 4;
+    }
+  }
+  if (notEvaluated == true) {
+    if (optCode == "03") {
+      if (parameterOne == "0")
+        posToReplace = inputArray[currentPos + 1];
+      else
+        posToReplace = currentPos + 1;
+
+      inputArray[posToReplace] = input;
+      //time to move on so...
+      console.log("made it to 3");
+
+      notEvaluated = false;
+      currentPos = currentPos + 2;
+      //console.log("CurrentPos = " + currentPos);
+      //console.log()
+    }
+  }
+  if (notEvaluated == true) {
+    if (optCode == "04") {
+      console.log("made it to 4");
+      if (parameterOne == "1") {
+        console.log("Output value = " + inputArray[currentPos + 1]);
+        if (inputArray[currentPos + 1] != 0)
+          break;
+      }
+      if (parameterOne == "0") {
+        valPositionOne = inputArray[currentPos + 1];
+        console.log("Output value = " + inputArray[valPositionOne]);
+        if (inputArray[valPositionOne] != 0)
+          break;
+      }
+
+
+      //else
+      //	console.log("Output value Pnot0= " + currentPos+1)
+      //time to move on so...
+
+      notEvaluated = false;
+      currentPos = currentPos + 2;
+    }
+  }
+  if (notEvaluated == true) {
+    console.log("Hmmm.  Ava we have a problem");
+    break;
+  }
+  console.log("still going...")
+  //Resetting Parameters to 0s
+  parameterOne = "0";
+  parameterTwo = "0";
+  parameterThree = "0";
+  parameterFour = "0";
+  //console.log(inputArray);
+}
+//console.log(inputArray);
+console.log("TEST Program finished...")
